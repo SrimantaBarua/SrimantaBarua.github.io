301362deb269b04ef24364bb0f2d0b50f7d623c9
Srimanta Barua <srimanta.barua1@gmail.com>
2020-05-05 23:33:12 +0530

    Remove extreneous right paren

--- core/script.js
+++ core/script.js
@@ -22,7 +22,7 @@ function display_projects() {
             const fncall = "load_project('" + proj_id + "','" + proj_name + "')"
             elem_html += '<div class="project" id="' + proj_id + '">'
             elem_html += '<div class="project-blurb"><h3 class="project-name">'
-            elem_html += '<a href="#" onClick="' + fncall + '")">';
+            elem_html += '<a href="#" onClick="' + fncall + '">';
             elem_html += projects[i]["name"] + '</a></h3><p class="project-langs">';
             const langs = projects[i]["langs"];
             if (langs.length > 0) {
