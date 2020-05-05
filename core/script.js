befbd196c13bcd25a87dfdec419ead7e965f3004
Srimanta Barua <srimanta.barua1@gmail.com>
2020-05-05 23:41:44 +0530

    More escaping

--- core/script.js
+++ core/script.js
@@ -2,7 +2,7 @@ function load_project(proj_id, proj_name) {
     //var xhr = new XMLHttpRequest();
     //xhr.open("GET", "api/project/" + proj_name);
     var elem = documents.getElementById(proj_id);
-    elem.innerHTML = "<div class='project-description'><p>Empty description</p></div>";
+    elem.innerHTML = '<div class="project-description"><p>Empty description</p></div>';
 }
 
 
@@ -18,13 +18,13 @@ function display_projects() {
         var elem_html = '';
         for (var i = 0; i < projects.length; i++) {
             const proj_id = 'project-num-' + i.toString();
-            const proj_name = projects[i].name;
+            const proj_name = projects[i].name.replace('\'', '\\\'').replace('"', '&quot;');
             const fncall = "load_project('" + proj_id + "','" + proj_name + "')"
             elem_html += '<div class="project" id="' + proj_id + '">'
             elem_html += '<div class="project-blurb"><h3 class="project-name">'
             elem_html += '<a href="#" onClick="' + fncall + '">';
-            elem_html += projects[i]["name"] + '</a></h3><p class="project-langs">';
-            const langs = projects[i]["langs"];
+            elem_html += projects[i].name + '</a></h3><p class="project-langs">';
+            const langs = projects[i].langs;
             if (langs.length > 0) {
                 elem_html += langs[0];
                 for (var j = 1; j < langs.length; j++) {
